229e5c8b1a224c8625b784a065f0002f3ecaebe6
jasimahmedq2 <jasimahmed90852417@gmail.com>
2023-08-10 02:40:05 +0600

    added walkthrough 3 screen

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
-import App from "../App";
+import WelcomePage1 from "../pages/welcome1";
+import Walkthrough1 from "../pages/Walkthrough1";
+import Walkthrough2 from "../pages/Walkthrough2";
+import Walkthrough3 from "../pages/Walkthrough3";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <WelcomePage1 />,
+  },
+  {
+    path: "/Walkthrough1",
+    element: <Walkthrough1 />,
+  },
+  {
+    path: "/Walkthrough2",
+    element: <Walkthrough2 />,
+  },
+  {
+    path: "/Walkthrough3",
+    element: <Walkthrough3 />,
   },
 ]);
 
